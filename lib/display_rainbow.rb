23111fa092a149ca5402451da8dfa633232add7a
Sara Walker <sarawall1187@gmail.com>
2018-11-13 17:41:12 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
 def display_rainbow(colors)
-  colors = ('R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet')
+  colors = #{'R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet'}
 end
-
